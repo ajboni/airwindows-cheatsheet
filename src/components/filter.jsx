86105867c3d5f0f7477d32bf84aaf8b8e6721faf
Alexis Boni <mail@aboni.dev>
2023-09-23 15:41:00 -0300

    PWA Test

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -16,7 +16,7 @@ export const Filter = ({ db, filter, setFilter, search, setSearch, sortBy, setSo
               key={category}
               className={`${
                 category === filter ? "bg-gray-500" : "bg-gray-700"
-              } col-span-1 flex rounded-md shadow-sm  px-2 py-1 cursor-pointer hover:bg-gray-600`}
+              } col-span-1 flex rounded-md shadow-sm  px-2 py-1 cursor-pointer hover:bg-gray-600 h-8 items-center`}
               onClick={() => setFilter(category)}
             >
               <div className="text-sm">{category}</div>
@@ -27,15 +27,15 @@ export const Filter = ({ db, filter, setFilter, search, setSearch, sortBy, setSo
 
       {/* Search */}
       <div className="relative rounded-md shadow-sm flex gap-2 text-sm">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-        </div>
         <div className="w-5/6 lg:w-5/6 flex relative  ">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+            <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </div>
           <input
             type="search"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className=" bg-gray-700 py-1 pr-6 pl-4 text-gray-200 px-3 focus:outline-none w-full rounded-md border-0"
+            className=" bg-gray-700  pr-6 pl-10 text-gray-200 px-3 ring-0  w-full rounded-md border-0 h-8 flex"
             placeholder="Search by name or description..."
           ></input>
           <button className="absolute cursor-pointer  right-1 top-0 bottom-0 " onClick={() => setSearch("")}>
@@ -44,9 +44,9 @@ export const Filter = ({ db, filter, setFilter, search, setSearch, sortBy, setSo
         </div>
 
         {/* Sort By */}
-        <div className="text-sm relative w-2/6 lg:-1/6">
+        <div className="text-sm relative w-2/6 xl:w-1/6">
           <Listbox value={sortBy} onChange={setSortBy}>
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-gray-700 py-1 pl-3 pr-10 text-left text-gray-200 shadow-sm ring-1 ring-inset ring-gray-600 md:text-sm md:leading-6 ">
+            <Listbox.Button className="h-8 border-none relative w-full cursor-default rounded-md bg-gray-700 py-1 pl-3 pr-10 text-left text-gray-200 shadow-sm md:text-sm md:leading-6 ">
               <span className="block truncate">{sortBy}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
@@ -67,14 +67,14 @@ export const Filter = ({ db, filter, setFilter, search, setSearch, sortBy, setSo
       {/* Mobile ListBox */}
       <div className="md:hidden text-sm relative">
         <Listbox value={filter} onChange={setFilter}>
-          <Listbox.Button className="relative w-full cursor-default rounded-md bg-gray-700 py-1.5 pl-3 pr-10 text-left text-gray-200 shadow-sm ring-1 ring-inset ring-gray-600 md:text-sm md:leading-6 ">
+          <Listbox.Button className="relative w-full cursor-default rounded-md bg-gray-700 py-1.5 pl-3 pr-10 text-left text-gray-200 shadow-sm  md:text-sm md:leading-6 ">
             <span className="block truncate">{filter}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
             </span>
           </Listbox.Button>
           <Listbox.Options
-            className={"bg-gray-700 p-4 rounded max-h-64 overflow-auto mt-4 z-30 left-0 right-0 shadow-xl border border-gray-600"}
+            className={"bg-gray-700 rounded max-h-64 overflow-auto mt-4 z-30 left-0 right-0 shadow-xl border border-gray-600"}
           >
             {categories.map((category) => (
               <Listbox.Option key={category} value={category} className="listbox-item">
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   base: "https://airwindowscheatsheet.aboni.dev/",
 });
