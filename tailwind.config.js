9136f8e08d2b6b8f9d257e74db7282ae5ff8a405
Alexis Boni <mail@aboni.dev>
2024-12-10 09:58:00 -0300

    Update dependencies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import typographyPlugin from "@tailwindcss/typography";
+
 /** @typ/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typographyPlugin],
 };
